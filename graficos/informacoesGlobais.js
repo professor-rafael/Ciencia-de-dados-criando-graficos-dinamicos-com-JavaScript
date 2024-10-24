7db03ebfc0963ea93348a2977de4ef9d57828e89
Rafael Assis Santos <101332916+professor-rafael@users.noreply.github.com>
2024-10-24 18:55:08 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = "https://github.com/professor-rafael/Ciencia-de-dados-criando-graficos-dinamicos-com-JavaScript"
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
