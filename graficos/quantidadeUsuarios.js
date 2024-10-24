d09767eec787761e2345d4ab5a3679909bfc2842
Rafael Assis Santos <101332916+professor-rafael@users.noreply.github.com>
2024-10-24 15:18:38 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,7 +1,7 @@
 import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const url = 'https://github.com/professor-rafael/Ciencia-de-dados-criando-graficos-dinamicos-com-JavaScript'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
