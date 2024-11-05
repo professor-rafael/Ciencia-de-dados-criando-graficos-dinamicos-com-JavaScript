18df3ea6a6f0325cf0a714154594c577819789c1
Rafael Assis Santos <101332916+professor-rafael@users.noreply.github.com>
2024-11-04 21:52:11 -0300

    r

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,7 +1,7 @@
 import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
-    const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json"
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
